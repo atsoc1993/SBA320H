1c5148f52fde38ebaaac5032024874910e10d68c
atsoc1993 <atsoc1993@gmail.com>
2025-07-08 10:10:40 -0400

    Adjusted readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# SEC Documents
+# U.S. Securities and Exchange Commission Document Application
 
 - The concept for this SBA revolves around publicly available, formal documents that businesses submit to the SEC
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios'
 
 function Home() {
+
+    useEffect(() => {
+        const getHomeInfo = async () => {
+            let response = axios.get('https://api.edgarfiling.sec.gov/status')
+            let data = response.data
+            console.log(data)
+        }
+
+        getHomeInfo();
+    }, [])
     return (
         <div>
-            <Link to='/'></Link>
+            <Link to='/' />
             <h1>Home</h1>
         </div>
     )
